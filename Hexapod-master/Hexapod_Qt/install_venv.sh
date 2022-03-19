e1322b10dc3a7005ce71d07c09f02aee33f2560c
EDP325 <olivier.jackson@hotmail.ca>
2022-03-18 21:45:14 -0400

    Libraries Update

--- Hexapod-master/Hexapod_Qt/install_venv.sh
+++ Hexapod-master/Hexapod_Qt/install_venv.sh
@@ -1,5 +1,5 @@
 python3 -m venv Qt_venv
 source "Qt_venv/bin/activate"
 pip install --upgrade pip
-pip install -r "requirements.txt"
-python3 -m ipykernel install --user --name=Qt_venv
+pip install -r "pip_requirements.txt"
+#python3 -m ipykernel install --user --name=Qt_venv
