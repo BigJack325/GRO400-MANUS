54741830c23a4a7afff53827e265aed1a3157646
Big Jack <72100463+EDP325@users.noreply.github.com>
2022-04-16 14:37:39 -0400

    Update install_venv.sh

--- Hexapod-master/Hexapod_Qt/install_venv.sh
+++ Hexapod-master/Hexapod_Qt/install_venv.sh
@@ -1,6 +1,6 @@
 python3 -m venv Qt_venv
 source "Qt_venv/bin/activate"
 pip install --upgrade pip
-apt-get install -y "apt_requirements_pi.txt"
+xargs -a apt_requirements_pi.txt sudo apt-get install
 pip install -r "pip_requirements.txt"
 python3 -m ipykernel install --user --name=Qt_venv
